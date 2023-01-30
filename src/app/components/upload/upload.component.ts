785cacf944d5e57ae1fb91e24a1ed19fd776ef2d
emmanueltejeda <emmanuel_tejeda@uri.edu>
2023-01-30 11:44:58 -0500

    Functioning Site

--- src/app/components/upload/upload.component.ts
+++ src/app/components/upload/upload.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { finalize } from 'rxjs';
+import { FileMetaData } from 'src/app/model/file-meta-data';
+import { FileService } from 'src/app/shared/file.service';
 
 @Component({
   selector: 'app-upload',
@@ -9,34 +11,103 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UploadComponent implements OnInit {
 
-  SERVER_URL = "http://localhost:8080/jazmyn/upload";
+  selectedFiles!: FileList;
+  currentFileUpload!: FileMetaData;
+  percentage: number = 0;
 
-  uploadForm!: FormGroup;  
+  listOfFiles: FileMetaData[] = [];
 
-  constructor(private formBuilder: FormBuilder, private httpClient: HttpClient) { }
+  constructor(private fileService: FileService, private fireStorage: AngularFireStorage, private modalService: NgbModal) {}
 
-  ngOnInit() {
-    this.uploadForm = this.formBuilder.group({
-      file: ['']
+  ngOnInit(): void {
+    this.getAllFiles();
+    this.percentage = 0;
+  }
+
+  selectFile(event: any){
+    this.selectedFiles = event.target.files;
+  }
+
+  uploadFile(){
+
+    this.currentFileUpload = new FileMetaData(this.selectedFiles[0]); 
+    const path = 'Uploads/' + this.currentFileUpload.file.name;
+
+    const storageRef = this.fireStorage.ref(path);
+
+    const uploadTask = storageRef.put(this.selectedFiles[0]);
+
+    uploadTask.snapshotChanges().pipe(finalize( () => {
+      storageRef.getDownloadURL().subscribe(downloadLink => {
+
+        this.currentFileUpload.url = downloadLink;
+        this.currentFileUpload.size = this.currentFileUpload.file.size;
+        this.currentFileUpload.name = this.currentFileUpload.file.name;
+
+        this.fileService.saveMetaDataOfFile(this.currentFileUpload);
+        this.ngOnInit();
+      })
+    })
+    ).subscribe((res: any) => {
+      this.percentage = (res.bytesTransferred * 100 / res.totalBytes);
+    }, err => {
+      console.log('Error Occured');
     });
+
   }
 
-  onFileSelect(event: any) {
-    if (event.target.files.length > 0) {
-      const file = event.target.files[0];
-      this.uploadForm!.get('file')?.setValue(file);
+  uploadHomeImage(){
+
+    this.currentFileUpload = new FileMetaData(this.selectedFiles[0]); 
+    const path = 'Home-Images/' + this.currentFileUpload.file.name;
+
+    const storageRef = this.fireStorage.ref(path);
+
+    const uploadTask = storageRef.put(this.selectedFiles[0]);
+
+    uploadTask.snapshotChanges().pipe(finalize( () => {
+      storageRef.getDownloadURL().subscribe(downloadLink => {
+
+        this.currentFileUpload.url = downloadLink;
+        this.currentFileUpload.size = this.currentFileUpload.file.size;
+        this.currentFileUpload.name = this.currentFileUpload.file.name;
+
+        this.fileService.addHomeImage(this.currentFileUpload);
+        this.ngOnInit();
+      })
+    })
+    ).subscribe((res: any) => {
+      this.percentage = (res.bytesTransferred * 100 / res.totalBytes);
+    }, err => {
+      console.log('Error Occured');
+    });
+
+  }
+
+  getAllFiles(){
+    this.fileService.getAllFiles().subscribe( res => {
+      this.listOfFiles = res.map((e: any) => {
+        const data = e.payload.doc.data();
+        data.id = e.payload.doc.id;
+        return data;
+      })
+    }, err => {
+      console.log('Error occured while fetching file meta data');
+    })
+  }
+
+  deleteFiles(file: FileMetaData){
+
+    if(window.confirm("Are you sure you want to delete" + file.name + '?')){
+      this.fileService.deleteFile(file);
+      this.ngOnInit;
+
     }
+
   }
 
-  onSubmit() {
-    const formData = new FormData();
-    formData.append('file', this.uploadForm?.get('file')?.value);
-
-    this.httpClient.post<any>(this.SERVER_URL, formData).subscribe(
-      (res) => console.log(res),
-      (err) => console.log(err)
-    );
-  }
-
+  openFullscreen(content) {
+		this.modalService.open(content, { fullscreen: true });
+	}
 
 }
